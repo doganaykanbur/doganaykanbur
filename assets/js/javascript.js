861ed3bafaac40b4967077230f6dc8d819a7aaa2
doganaykanbur <kanburdoganay@gmail.com>
2024-11-06 02:47:41 +0300

    files are created

---
+++ assets/js/javascript.js
@@ -0,0 +1,13 @@
+function saatigoster() {
+  const date = new Date();
+  const saat = date.getHours().toString().padStart(2, "0");
+  const dakika = date.getMinutes().toString().padStart(2, "0");
+  const saniye = date.getSeconds().toString().padStart(2, "0");
+
+  const timeString = `${saat}:${dakika}:${saniye}`;
+  document.getElementById("saat").textContent = timeString;
+}
+
+setInterval(saatigoster, 1000);
+
+saatigoster();
